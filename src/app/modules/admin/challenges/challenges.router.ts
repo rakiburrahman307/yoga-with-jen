f361adc6efe05da0c562a4ed83c1251784740a73
rakiburrahman307 <rakiburrahman307@gmail.com>
2025-08-25 21:38:23 +0600

    refactor(challenges): remove file upload middleware from challenge routes
    The file upload middleware was removed from the challenge creation and update routes as it's no longer needed. This simplifies the routes and reduces unnecessary dependencies.

--- src/app/modules/admin/challenges/challenges.router.ts
+++ src/app/modules/admin/challenges/challenges.router.ts
@@ -1,22 +1,13 @@
 import express from 'express';
 import auth from '../../../middleware/auth';
 import { USER_ROLES } from '../../../../enums/user';
-import validateRequest from '../../../middleware/validateRequest';
-import fileUploadHandlerbunny from '../../../middleware/fileUploadHandlerbunny';
 import { ChallengeController } from './challenges.controller';
-import { CreateDailyInspiration } from './challenges.validation';
+
 
 const router = express.Router();
 
-// Route to create a new "Create Challenge" entry
-router.post(
-     '/create',
-     auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN),
-     fileUploadHandlerbunny,
-     validateRequest(CreateDailyInspiration.createDailyInspiration),
-     ChallengeController.createChallengeVideos,
-);
-router.post('/schedule-create', auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN), fileUploadHandlerbunny, ChallengeController.createChallengeForSchedule);
+
+router.post('/schedule-create', auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN), ChallengeController.createChallengeForSchedule);
 // Route to get all "Create Challenge" entries
 router.get('/latest', auth(USER_ROLES.USER), ChallengeController.getChallenges);
 router.get('/single/:id', auth(USER_ROLES.USER, USER_ROLES.SUPER_ADMIN), ChallengeController.singleChallenge);
@@ -27,7 +18,7 @@ router.post('/mark-video-watched/:videoId', auth(USER_ROLES.USER), ChallengeCont
 // Route to get a specific "Create Challenge" entry by ID
 router.get('/:id', auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN), ChallengeController.singleChallenge);
 
-router.patch('/:id', auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN), fileUploadHandlerbunny, ChallengeController.updateChallenge);
+router.patch('/:id', auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN), ChallengeController.updateChallenge);
 
 // Route to delete a "Create Challenge" entry by ID
 router.delete('/:id', auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN), ChallengeController.deleteChallenge);
