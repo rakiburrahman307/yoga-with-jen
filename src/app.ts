6694d61d738f8e7002d0e48f4e20fa3cfd4162fc
rakiburrahman307 <rakiburrahman307@gmail.com>
2025-09-02 12:01:13 +0600

    refactor(app): use dynamic upload directory from config
    Switch to development mode in .env and clean up old log files

--- src/app.ts
+++ src/app.ts
@@ -43,7 +43,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //file retrieve
-app.use(express.static('uploads'));
+const baseUploadDir = getUploadDirectory();
+app.use(express.static(baseUploadDir));
 
 //router
 app.use('/api/v1', router);
